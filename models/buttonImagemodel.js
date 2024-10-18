78c49c3d61e6a04bd17dc08020e8a3f5a1f038b6
infocvzone <info@computervision.zone>
2024-10-18 13:03:19 +0500

    + added

--- models/buttonImagemodel.js
+++ models/buttonImagemodel.js
@@ -1,17 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const buttonImageSchema = new mongoose.Schema({
-    id: { type: Number, default: Date.now()},
-    type: { type: String, default: 'ButtonImage' },
+const buttonImageSchema = new mongoose.Schema(
+  {
+    id: { type: Number, default: Date.now() },
+    type: { type: String, default: "ButtonImage" },
     x: { type: Number, default: 150 },
     y: { type: Number, default: 150 },
-    scale: { type: Number, default: 0.5 },
-    text: { type: String, default: "Submit" },
     idleImage: { type: String, required: true },
     hoverImage: { type: String, required: true },
     clickedImage: { type: String, required: true },
-    textColor: { type: String, default: '#FFFFFF' },
-}, { timestamps: true });
+    scale: { type: Number, default: 0.5 },
+    text: { type: String, default: "Submit" },
+    text_anchor: { type: String, default: "center" },
+    textColor: { type: String, default: "#000000" },
+    fontFamily: { type: String, default: "Roboto-Bold" },
+    fontSize: { type: Number, default: 16 },
+    onClick: { type: String, default: null }, // Store function names or references as strings
+    onRelease: { type: String, default: null },
+    onHover: { type: String, default: null },
+    name: { type: String, default: null },
+  },
+  { timestamps: true }
+);
 
-const ButtonImage = mongoose.model('ButtonImage', buttonImageSchema);
+const ButtonImage = mongoose.model("ButtonImage", buttonImageSchema);
 module.exports = ButtonImage;
