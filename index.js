14a074bb78fe4911767c0d3624777aecd74df35b
infocvzone <info@computervision.zone>
2024-11-14 14:56:01 +0500

    variableName

--- index.js
+++ index.js
@@ -24,15 +24,21 @@ const fs = require("fs");
 const PORT = process.env.PORT || 5000;
 connectDB();
 
+
+
 // Middleware
 app.use(cors());
 app.use(express.json({ limit: "50mb", extended: false }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
+
+
 // Routes
 app.get("/", (req, res) => {
   res.json({ message: "hello py-visual..." });
 });
+
+
 app.use("/api/users", userRoutes);
 app.use("/api/admins", adminRoutes);
 app.use("/api/types", typeRoutes);
@@ -99,10 +105,12 @@ app.get('/api/image/:filename', (req, res) => {
 
 */
 
+
 // Error handling for undefined routes
 app.use((req, res, next) => {
   res.status(404).json({ message: "API endpoint not found" });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
